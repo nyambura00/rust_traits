33546f78122c579b35ccad5f90e4369823414020
nyambura00 <winnienyambura14@gmail.com>
2023-10-02 12:48:49 +0300

    enforce error handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-// TASK: create a basic banking system using Traits in Rust. 
+// TASK 1: Create a basic banking system using Traits in Rust. 
 // The program will allow users to create accounts, deposit and withdraw money, and view their account balance.
+
+// TASK 2: Enforce error handling
 
 fn main() {
     let mut alice_account = BankAccount {
         account_number: 12345,
         holder_name: "Alice".to_string(),
-        balance: 0.22
+        balance: 20.22
     };
 
     let mut bob_account = BankAccount {
@@ -15,11 +17,17 @@ fn main() {
     };
 
     println!("Alice's balance before deposit: {}.", alice_account.balance);
-    println!("Withdraw 400 from Bob: {}", bob_account.balance);
+    println!("Bob's balance before withdrawal: {}", bob_account.balance);
 
-    alice_account.deposit(200.12);
+    match alice_account.deposit(200.12) {
+        Ok(_) => println!("Deposit Status: OK"),
+        Err(err) => println!("Deposit Status: FAILED, Err: {:?}", err)
+    };
 
-    bob_account.withdraw(400.0);
+    match bob_account.withdraw(400.0) {
+        Ok(_) => println!("Withdrawal Status: OK"),
+        Err(err) => println!("Withdrawal Status: FAILED, Err: {:?}", err)
+    };
 
     println!("Alice's balance after deposit: {:#?}", alice_account.balance);
     println!("Bob's balance after withdrawal: {:#?}", bob_account.balance);
@@ -30,19 +38,48 @@ struct BankAccount {
     holder_name: String,
     balance: f64
 }
+
+#[derive(Debug)]
+enum BankTransferErrors {
+    InvalidAmount(String),
+    ThresholdLimitReached(String),
+    InvalidDataType,
+    CustomError(String)
+}
+
 trait Account {
-   fn deposit(&mut self, amount: f64);
-   fn withdraw(&mut self, amount: f64);
+   fn deposit(&mut self, amount: f64) -> Result<(), String>;
+   fn withdraw(&mut self, amount: f64) -> Result<(), String>;
    fn balance(&mut self);
 }
 
 impl Account for BankAccount {
-    fn deposit(&mut self, amount: f64) {
-        self.balance = self.balance + amount
+    fn deposit(&mut self, amount: f64) -> Result<(), String> {
+        if amount < 100.00 {
+            return Err(format!("{:#?}", BankTransferErrors::InvalidAmount("Minimum Deposit Amount = 100.00".to_string())));
+        } else if !amount.is_finite() {
+            return Err(format!("{:#?}", BankTransferErrors::InvalidDataType));
+        } else {
+            self.balance += amount;
+        }
+
+        Ok(())
+
     }
 
-    fn withdraw(&mut self, amount: f64) {
-        self.balance = self.balance - amount
+    fn withdraw(&mut self, amount: f64) -> Result<(), String> {
+        if self.balance > 10.00 {
+            if self.balance - amount < 10.00 {
+                return Err(format!("{:#?}", BankTransferErrors::ThresholdLimitReached("Minimum amount needed in the account = 10.0".to_string())));
+            } else if amount < 100.00 {
+                return Err(format!("{:#?}", BankTransferErrors::InvalidAmount("Minimum Withdrawal Amount = 100.00".to_string())));
+            }
+        } else {
+            return Err(format!("{:#?}", BankTransferErrors::CustomError("Account is running low. Cannot withdraw, deposit now".to_string())));
+        }
+        
+        Ok(())
+
     }
 
     fn balance(&mut self) {
